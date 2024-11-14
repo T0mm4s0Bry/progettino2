e9c23696683d4c4103e432dbf608876016b8cb9d
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-11-14 07:55:28 +0000

    aggiunto "aggiungi"

---
+++ ListaSpesa.py
@@ -0,0 +1,9 @@
+Listaspesa = [""]
+def aggiungi(): 
+    spesa = int(input("inserisci elemento alla lista della spesa: "))
+    for i in range (spesa):
+        prodotti = input("inserisci il prodotto: ")
+        Listaspesa.append(prodotti)
+
+
+                        
