f00bff7ef886eff55279cc2003974fe633c2b43a
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-11-19 08:45:32 +0000

    fatto punto 1 e 2 della 3°fase

--- ListaSpesa_flask/App.py
+++ ListaSpesa_flask/App.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
+#il request servirà per mandare la richiesta all'utente redirect per reindirizzarlo correttamente e url_for servirà per il link
 
 app = Flask(__name__)
+
+Lista_Spesa = []
 
 @app.route('/')
 def home():
